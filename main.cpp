654d42223d751cab412bd016f24079753709d1e2
VinothKumar210 <vinothkumarvk6669@gmail.com>
2026-09-15 10:35:12 +0530

    Add subtraction feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 using namespace std;
+float Subtraction(float a, float b){
+    return a-b;
+}
 
 int main() {
     cout << "Simple Calculator" << endl;
+    cout << Subtraction(10, 20) << endl;
     return 0;
 }
