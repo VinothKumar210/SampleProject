0316a525be02f40bde92c99fe2a40cc4476d59e1
VinothKumar210 <vinothkumarvk6669@gmail.com>
2026-09-15 10:36:29 +0530

    Add multiplication feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+float Multiplication(float a,float b){
+    return a*b;
+}
 int main() {
     cout << "Simple Calculator" << endl;
+    cout << Multiplication(10, 20) << endl;
     return 0;
 }
